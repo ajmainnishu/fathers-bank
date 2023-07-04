7c3dad7fb23d0d12c0beb842d64c0b958394751e
Ajmain Nishu <ajmainnishu@gmail.com>
2023-07-05 03:09:26 +0600

    Bug fixed

--- withdraw.js
+++ withdraw.js
@@ -13,21 +13,17 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawTotal = document.getElementById('withdraw-total');
     const withdrawTotalString = withdrawTotal.innerText;
     const newWithdrawTotal = parseFloat(withdrawTotalString);
-    const totalWithdrawAmount = newWithdrawField + newWithdrawTotal;
-    withdrawTotal.innerText = totalWithdrawAmount;
 
     // balance display part
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalString = balanceTotal.innerText;
     const newBalanceTotal = parseFloat(balanceTotalString);
-    if (newBalanceTotal == 0) {
-        alert('Your balance 0, please deposit first')
-        return
-    }
-    else if (newBalanceTotal < newWithdrawField) {
-        alert('Please provide a less then total balance');
+    if (newWithdrawField > newBalanceTotal) {
+        alert('Please provide number below total balance');
         return;
     }
+    const totalWithdrawAmount = newWithdrawField + newWithdrawTotal;
+    withdrawTotal.innerText = totalWithdrawAmount;
     const totalBalanceAmount = newBalanceTotal - newWithdrawField;
     balanceTotal.innerText = totalBalanceAmount;
 })
